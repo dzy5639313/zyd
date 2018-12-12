9ecee53a9ed91d585bfe50c7c0fdbd3c72df1f23
solomon <dzy5639313@163.com>
2018-12-12 22:26:35 +0800

    修改构建脚本

--- build.sh
+++ build.sh
@@ -1,4 +1,30 @@
-docker build -t zyd-eureka ./eureka/
-docker build -t zyd-config ./config/
-docker build -t zyd-gateway ./gateway/
-docker build -t zyd-api ./api/
+repo_url=registry-vpc.cn-hangzhou.aliyuncs.com
+namespace=dzy5639313
+
+docker login --username=13666031483 --password=wym68689 $repo_url
+
+p=eureka
+docker build -t zyd-$p ./$p/
+docker tag zyd-$p $repo_url/$namespace/zyd-$p
+docker push $repo_url/$namespace/zyd-$p
+docker rmi $repo_url/$namespace/zyd-$p
+
+p=config
+docker build -t zyd-$p ./$p/
+docker tag zyd-$p $repo_url/$namespace/zyd-$p
+docker push $repo_url/$namespace/zyd-$p
+docker rmi $repo_url/$namespace/zyd-$p
+
+p=gateway
+docker build -t zyd-$p ./$p/
+docker tag zyd-$p $repo_url/$namespace/zyd-$p
+docker push $repo_url/$namespace/zyd-$p
+docker rmi $repo_url/$namespace/zyd-$p
+
+p=api
+docker build -t zyd-$p ./$p/
+docker tag zyd-$p $repo_url/$namespace/zyd-$p
+docker push $repo_url/$namespace/zyd-$p
+docker rmi $repo_url/$namespace/zyd-$p
+
+docker logout $repo_url
